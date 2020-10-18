dbd6afc6adb68a86d12a7256066837e6f0112317
fmartin <felix.martin@bergwerk-it.de>
2020-10-18 12:44:04 +0200

    Adding rudimentary docker stack.

---
+++ docker/froxlor/userdata.inc.php
@@ -0,0 +1,12 @@
+<?php
+// automatically generated userdata.inc.php for Froxlor
+$sql['host']='froxlor_db';
+$sql['user']='froxlor';
+$sql['password']='froxlor';
+$sql['db']='froxlor';
+$sql_root[0]['caption']='Default';
+$sql_root[0]['host']='froxlor_db';
+$sql_root[0]['user']='root';
+$sql_root[0]['password']='root';
+// enable debugging to browser in case of SQL errors
+$sql['debug'] = false;
